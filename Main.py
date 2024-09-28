c6202f5f0b02297531c0ff74bc256023e3d67dc2
Yeong-Hyun-Kim <tim0722@dgu.ac.kr>
2024-09-28 23:40:17 +0900

    ossp1 upload 2024-09-28

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
 Name = input("이름을 입력하세요: ")
 Number = input("학번을 입력하세요: ")
+Major = input("학과를 입력하세요: ")  
 
+print("\n")
 print("<출력>")
 print("이름:", Name)
 print("학번:", Number)
+print("학과:", Major)
