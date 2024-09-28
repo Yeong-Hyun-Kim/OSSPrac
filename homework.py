68bea33d722c59860afcb79ea79a9fdaec13e77c
Yeong-Hyun-Kim <tim0722@dgu.ac.kr>
2024-09-29 02:45:04 +0900

    git conflict homework.py

---
+++ homework.py
@@ -0,0 +1,11 @@
+Name = input("이름을 입력하세요: ")
+Number = input("학번을 입력하세요: ")
+Major = input("학과를 입력하세요: ")  
+Grade = input("학년을 입력하세요: ")  
+
+print("\n")
+print("<출력>")
+print("이름:", Name)
+print("학번:", Number)
+print("학과:", Major)
+print("학교:", Grade)
